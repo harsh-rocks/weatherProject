96f6e31fe42216ab0abb3aac2c2f04af1c865f71
Harsh Raj <harshraj815317@gmail.com>
2021-10-06 17:51:39 +0530

    comit all

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+
+// This makes a folder name public static so that it is available for index.html
+app.use(express.static(__dirname+"/public"));
+app.use(express.urlencoded({extended:true}));
+
+app.get("/:cityName",(req,res)=>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.params.cityName}&appid=${process.env.API_KEY}`
+    https.get(url,
+    response=>{
+        console.log(response.statusCode);
+        response.on("data",function(data){
+            const weatherInfo = JSON.parse(data);
+            res.json(weatherInfo);
+        })
+    })
+        
+    
+});
+
+const port = process.env.PORT || '3000';
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`);
+})
---
+++ public/app.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+const port = process.env.PORT || '3000';
+button.addEventListener('click', function(name){
+fetch(`http://localhost:${port}/${input.value}`)
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
