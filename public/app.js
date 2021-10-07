106dbc0ef9404a580234ef22eaa05111b59b1c1a
Harsh Raj <harshraj815317@gmail.com>
2021-10-07 18:25:44 +0530

    for localHost

--- public/app.js
+++ public/app.js
@@ -6,9 +6,9 @@ var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
-// fasdfasdfasdfasf
+
 button.addEventListener('click', function(name){
-fetch(`https://warm-ravine-82081.herokuapp.com/${input.value}`)
+fetch(`http://localhost:3000/${input.value}`)
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
