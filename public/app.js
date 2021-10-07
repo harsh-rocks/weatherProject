5cf59794e0cee78ee8600c9119df776af8491f2a
Harsh Raj <harshraj815317@gmail.com>
2021-10-07 18:09:44 +0530

    fix require

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ var button= document.querySelector('.submit');
 
 
 button.addEventListener('click', function(name){
-fetch(`https://warm-ravine-82081.herokuapp.com//${input.value}`)
+fetch(`https://warm-ravine-82081.herokuapp.com/${input.value}`)
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
