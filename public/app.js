3a0d4d482704afb579230b54e1762c74014d5bad
Harsh Raj <harshraj815317@gmail.com>
2021-10-07 18:07:00 +0530

    changed frontend app.js to

--- public/app.js
+++ public/app.js
@@ -6,9 +6,9 @@ var desc = document.querySelector('.desc');
 var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.submit');
 
-const port = process.env.PORT || '3000';
+
 button.addEventListener('click', function(name){
-fetch(`http://localhost:${port}/${input.value}`)
+fetch(`https://warm-ravine-82081.herokuapp.com//${input.value}`)
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp'];
